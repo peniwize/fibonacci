f7eeb509cc1bf6efaceaf4fb0c1260b13e0d1878
Matt Ervin <matt@impsoftware.org>
2023-12-17 00:21:33 -0800

    Simple recursive Fibonacci algorithm.

--- main.cpp
+++ main.cpp
@@ -23,140 +23,33 @@ using namespace std;
 // [----------------(120 columns)---------------> Module Code Delimiter <---------------(120 columns)----------------]
 
 /*
-    Iterative solution:
+    Recursive solution:
         
         Time = O(2^n), because each fibonacci number is calculated
                        from TWO previous fibonacci numbers, e.g.
-                       fib(5) =
-                                                   5-00
-                                                  /    \
-                                           +-----+      +----+
-                                          /                   \
-                                         4-01                  3-02
-                                     /         \              /    \
-                                    3-03        2-04         2-05   1-06
-                                   /    \      /    \       /    \
-                                  2-07   1-08 1-09   0-10  1-11   0-12
-                                 /    \
-                                1-13   0-14
-
+                       fib(4) =
+                                       4
+                                     /   \
+                                    3     2
+                                   / \   / \
+                                  2   1 1   0
+                                 / \
+                                1   0
                        Therefore the maximum possible number of 
-                       nodes in the tree is 2^n-1 (2^5-1 == 31, in this case.)
-                       The label format of nodes above is: fibonacci_number-n,
-                       where 'n' is a unique node ID used to identify each 
-                       individual node.
+                       calculations is 2^n-1 (2^4-1 == 15, in this case.)
+                       Note that there will always be fewer because the 
+                       0 and 1 base cases do not need to be calculated
+                       and cause the binary tree to be "imperfect".
         
-        Space = O(1), because the maximum depth of the tree is Log2(31)
-                      and 31 is constant.
+        Space = Log2(n), which is the maximum depth of the tree and 
+                         therefore the maximum number of recursive calls
+                         that will be made.
 */
 class Solution {
 public:
-    /*
-        Stack based algorithm:
-            
-        result = 0
-        
-        push 5-00            [stack=5-00]
-
-        pop 5-00             [stack=empty]
-        push 3-02 (right)    [stack=3-02]
-        push 4-01 (left)     [stack=4-01]
-        
-        // Begin traverstal of the left side of the binary tree.
-        
-        pop 4-01             [stack=3-02]
-        push 2-04 (right)    [stack=3-02 2-04]
-        push 3-03 (left)     [stack=3-02 2-04 3-03]
-        
-        pop 3-03             [stack=3-02 2-04]
-        push 1-08 (right)    [stack=3-02 2-04 1-08]
-        push 2-07 (left)     [stack=3-02 2-04 1-08 2-07]
-        
-        pop 2-07             [stack=3-02 2-04 1-08]
-        push 0-14 (right)    [stack=3-02 2-04 1-08 0-14]
-        push 1-13 (left)     [stack=3-02 2-04 1-08 0-14 1-13]
-        
-        pop 1-13             [stack=3-02 2-04 1-08 0-14]
-        // NOTE: No new value to push because last popped value < 2.
-        result += 1 = 1
-
-        pop 0-14             [stack=3-02 2-04 1-08]
-        // NOTE: No new value to push because last popped value < 2.
-        result += 0 = 1
-
-        pop 1-08             [stack=3-02 2-04]
-        // NOTE: No new value to push because last popped value < 2.
-        result += 1 = 2
-
-        pop 2-04             [stack=3-02]
-        push 1-00 (right)    [stack=3-02 1-09]
-        push 0-10 (left)     [stack=3-02 1-09 0-10]
-        
-        pop 0-10             [stack=3-02 1-09]
-        // NOTE: No new value to push because last popped value < 2.
-        result += 0 = 2
-
-        pop 1-09             [stack=3-02]
-        // NOTE: No new value to push because last popped value < 2.
-        result += 1 = 3
-
-        // This completes the traversal of the left side of the binary tree.
-        // Now begin traversal of the right side of the binary tree.
-
-        pop 3-02             [stack=empty]
-        push 1-06 (right)    [stack=1-06]
-        push 2-05 (left)     [stack=1-06 2-05]
-
-        pop 2-05             [stack=1-06]
-        push 0-12            [stack=1-06 0-12]
-        push 1-11            [stack=1-06 0-12 1-11]
-
-        pop 1-11             [stack=1-06 0-12]
-        // NOTE: No new value to push because last popped value < 2.
-        result += 1 = 4
-
-        pop 0-12             [stack=1-06]
-        // NOTE: No new value to push because last popped value < 2.
-        result += 0 = 4
-
-        pop 1-06             [stack=empty]
-        result += 1 = 5
-
-        // NOTE: result == 5.
-
-        // This completes the traversal of the right side of the binary tree.
-        // The stack is empty so all nodes have been visited and the algorithm is complete.
-    */
-
-// Helper macros for fixed size stack (built on an array).
-#define push(VAL) (cout << "push(" << VAL << ")\n", assert(/*std::size(stack)*/n > tos), stack[tos++] = (VAL))
-#define pop() (assert(0 < tos), cout << "pop(" << stack[tos - 1] << ")\n", stack[--tos])
-#define stack_empty() (0 == tos)
-#define add_to_result(VAL) (cout << result << " + " << VAL << " = " << (result + VAL) << "\n", result += (VAL))
-
     template <typename T>
-    T fib(T n) const {
-        T result{};
-
-        if (1 < n) {
-            T stack[n]; // Fast allocation; no heap.
-            size_t tos{}; // Index of next available location (last element + 1).
-            
-            push(n);
-            while (!stack_empty()) {
-                auto const fibnum = pop();
-                if (1 < fibnum) {
-                    push(fibnum - 2);
-                    push(fibnum - 1);
-                } else {
-                    add_to_result(fibnum);
-                }
-            }
-        } else {
-            result = n;
-        }
-
-        return result;
+    constexpr T fib(T n) {
+        return 1 < n ? fib(n - 1) + fib(n - 2) : n;
     }
 };
 
